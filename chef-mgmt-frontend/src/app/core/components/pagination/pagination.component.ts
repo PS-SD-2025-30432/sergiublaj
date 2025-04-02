c848aec95c2e9d7cb3fad4d1493140b643c6dcff
sergiublaj <sergiublaj01@gmail.com>
2025-04-02 13:24:15 +0300

    enhanced pagination

--- chef-mgmt-frontend/src/app/core/components/pagination/pagination.component.ts
+++ chef-mgmt-frontend/src/app/core/components/pagination/pagination.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Router } from '@angular/router';
 
 
@@ -8,23 +8,23 @@ import { Router } from '@angular/router';
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss'
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent {
   @Input() totalPages: number = 1;
   @Input() currentPage: number = 0;
   @Output() pageChange: EventEmitter<number> = new EventEmitter<number>();
-
-  pages: number[] = [];
 
   constructor(private router: Router) {
     this.router = router;
   }
 
-  ngOnInit(): void {
-    this.pages = Array.from({ length: this.totalPages }, (_, i) => i);
+  *pageRange(): Iterable<number> {
+    for (let i = 0; i < this.totalPages; i++) {
+      yield i;
+    }
   }
 
   changePage(page: number): void {
-    if (page < 0 || page > this.totalPages - 1|| page === this.currentPage) {
+    if (page < 0 || page > this.totalPages - 1 || page === this.currentPage) {
       return;
     }
 
